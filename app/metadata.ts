0ed69b816e76116a690117f2d07a1f60302469f2
Martin Horňak <martin.hornak1997@gmail.com>
2025-07-08 15:27:49 +0200

    Add Google Search Console verification meta tag

--- app/metadata.ts
+++ app/metadata.ts
@@ -3,4 +3,7 @@ import { Metadata } from 'next'
 export const metadata: Metadata = {
   title: "Martin Horňak - UX Designer",
   description: "UX designer focused on internal tools and design systems.",
+  verification: {
+    google: "nPzgvjldolAr8IEoMHv2VP6zDpXNYcaIRSGuVG7wNY"
+  }
 }; 
